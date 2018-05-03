3a1689a02610fc23e52df459cd211f4d5f11dfb7
Pieter Baecke <pieter@zetom.nl>
2018-05-03 15:01:30 +0200

    change decimals for fintrans

--- fintrans.go
+++ fintrans.go
@@ -5,6 +5,8 @@ package multivers
 import (
 	"context"
 	"fmt"
+
+	"github.com/shopspring/decimal"
 )
 
 const (
@@ -55,23 +57,23 @@ type FinTrans struct {
 type FinTransEntries []FinTransEntry
 
 type FinTransEntry struct {
-	Type                       string  `json:"$type,omitempty"`                      // "UNIT4.Multivers.API.BL.Financial.Edit.BasicEntryProxy, UNIT4.Multivers.API.Web.WebApi.Model",
-	AccountID                  string  `json:"accountId,omitempty"`                  // "4500",
-	CostCentreID               string  `json:"costCentreId,omitempty"`               //"",
-	CostCentreIDRequired       bool    `json:"costCentreIdRequired,omitempty"`       //false
-	CostUnitID                 string  `json:"costUnitId,omitempty"`                 //""
-	CostUnitIDRequired         bool    `json:"costUnitIdRequired,omitempty"`         //false,
-	CreditAmount               float64 `json:"creditAmount"`                         //100.00,
-	DebitAmount                float64 `json:"debitAmount"`                          //0.0,
-	Description                string  `json:"description,omitempty"`                //"Test Basicentry",
-	Document                   string  `json:"document,omitempty"`                   //"",
-	NonDeductibleVatPercentage float64 `json:"nondeductibleVatPercentage,omitempty"` // 0.0,
-	Quantity                   float64 `json:"quantity,omitempty"`                   // 0.0,
-	TransactionDate            string  `json:"transactionDate,omitempty"`            // "28-2-2018",
-	VatAmount                  float64 `json:"vatAmount,omitempty"`                  // 17.36,
-	VatChanged                 bool    `json:"vatChanged,omitempty"`                 // false,
-	VatCodeID                  int     `json:"vatCodeId,omitempty"`                  // 2,
-	VatIncluded                bool    `json:"vatIncluded,omitempty"`                // true,
-	VatScenarioID              int     `json:"vatScenarioId,omitempty"`              // 6,
-	VatType                    int     `json:"vatType,omitempty"`                    // 0
+	Type                       string          `json:"$type,omitempty"`                      // "UNIT4.Multivers.API.BL.Financial.Edit.BasicEntryProxy, UNIT4.Multivers.API.Web.WebApi.Model",
+	AccountID                  string          `json:"accountId,omitempty"`                  // "4500",
+	CostCentreID               string          `json:"costCentreId,omitempty"`               //"",
+	CostCentreIDRequired       bool            `json:"costCentreIdRequired,omitempty"`       //false
+	CostUnitID                 string          `json:"costUnitId,omitempty"`                 //""
+	CostUnitIDRequired         bool            `json:"costUnitIdRequired,omitempty"`         //false,
+	CreditAmount               decimal.Decimal `json:"creditAmount"`                         //100.00,
+	DebitAmount                decimal.Decimal `json:"debitAmount"`                          //0.0,
+	Description                string          `json:"description,omitempty"`                //"Test Basicentry",
+	Document                   string          `json:"document,omitempty"`                   //"",
+	NonDeductibleVatPercentage float64         `json:"nondeductibleVatPercentage,omitempty"` // 0.0,
+	Quantity                   float64         `json:"quantity,omitempty"`                   // 0.0,
+	TransactionDate            string          `json:"transactionDate,omitempty"`            // "28-2-2018",
+	VatAmount                  decimal.Decimal `json:"vatAmount,omitempty"`                  // 17.36,
+	VatChanged                 bool            `json:"vatChanged,omitempty"`                 // false,
+	VatCodeID                  int             `json:"vatCodeId,omitempty"`                  // 2,
+	VatIncluded                bool            `json:"vatIncluded,omitempty"`                // true,
+	VatScenarioID              int             `json:"vatScenarioId,omitempty"`              // 6,
+	VatType                    int             `json:"vatType,omitempty"`                    // 0
 }
