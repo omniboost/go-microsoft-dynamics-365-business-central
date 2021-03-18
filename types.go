24b3fee0e1d3aef1d06eab5542e44f427dbf95b1
Leon Bogaert <leon@tim-online.nl>
2021-03-18 16:26:38 +0100

    Added VatCategory_GetAllVatCategories operation

--- types.go
+++ types.go
@@ -15,3 +15,33 @@ type Account struct {
 	LastModifiedDateTime string `json:"lastModifiedDateTime"`
 	Currency             string `json:"currency,omitempty"`
 }
+
+type VATCategories []VATCategory
+
+type VATCategory struct {
+	VATCategoryID        string           `json:"vatCategoryId"`
+	Description          string           `json:"description"`
+	Active               bool             `json:"active"`
+	ExcludeListedTaxes   bool             `json:"excludeListedTaxes"`
+	LastModifiedDateTime string           `json:"lastModifiedDateTime"`
+	VATCategoryLines     VATCategoryLines `json:"vatCategoryLines"`
+}
+
+type VATCategoryLines []VATCategoryLine
+
+type VATCategoryLine struct {
+	VATID        string   `json:"vatId"`
+	Description  string   `json:"description"`
+	Type         string   `json:"type"`
+	CalculateOn  string   `json:"calculateOn"`
+	CashDiscount string   `json:"cashDiscount"`
+	VATRates     VATRates `json:"vatRates"`
+}
+
+type VATRates []VATRate
+
+type VATRate struct {
+	RevisionID string  `json:"revisionID"`
+	VATRate    float64 `json:"vatRate"`
+	StartDate  string  `json:"startDate"`
+}
